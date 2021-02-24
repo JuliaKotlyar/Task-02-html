2bc3eb52c623caeea86a65bdadd5cfbd3928d8cd
JuliaKotlyar <julia.kotlyar2014@gmail.com>
2021-02-24 16:59:28 +0200

    change html css js

--- js/index.js
+++ js/index.js
@@ -3,3 +3,10 @@ function updateDateTime() {
   const dateTime = document.querySelector('.date-time');
   dateTime.innerText = new Date().toLocaleString();
 }
+
+function openNav() {
+  documen.getElementByClassName('.menu').style.width = '100%';
+}
+function closeNav() {
+  document.getElementsByClassName('.menu').style.width = '0';
+}
